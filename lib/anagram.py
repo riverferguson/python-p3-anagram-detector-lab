d0e7b596d0d7608913adea7d0cd1c92b55405fe1
River Ferguson <riverferguson@gmail.com>
2023-05-24 17:14:47 -0700

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.letters = sorted([letter for letter in word])
+    
+    def match(self, words):
+        word_list = []
+        for word in words:
+            if sorted([letter for letter in word]) == self.letters:
+                word_list.append(word)
+        return word_list
